3b5e5e40fdc4831ebc1a0da91b59bbc213b6f970
chaithanyarlk <chithanyarlk@gmail.com>
2020-03-15 20:51:24 +0530

    changes

--- Shaker_Sort/Shaker_Sort.py
+++ Shaker_Sort/Shaker_Sort.py
@@ -1,48 +1,52 @@
-#Python 3 implemnetation of Shaker_Sort
+# Python implementation of Shaker Sort
 
-def Shaker_Sort(arr):
-     n = len(arr)
-     #getting the length of the listto get the end
-     begin = 0
-     end = n - 1
-     swapped = True
-     #if swapped is false then we can stop the sorting as the elements in the list all are in sorted order
+def Shaker_Sort(arr) :
+    n = len(arr)
+    # Getting the length of the list 
+    begin = 0
+    end = n - 1
+    swapped = True
+    # If swapped is False then we have to stop sorting as list is aldready sorted
+    while swapped == True :
+        # Continue until it is False
+        for i in range (begin, end, 1):
+            if arr[i] > arr[i + 1]:
+                # Left element is greater than that of right in list
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                # Swapping the elements of the list
+                swapped = True
+        if swapped == False:
+            break
+            # If list is sorted then have to break the loop
+        swapped = False
+        # Now apply bubble sort in opposite direction as list is not sorted
+        # Leave the lost element iterated from above loop as it is in correct place
+        for i in range (end - 1, begin - 1, - 1) :
+            if arr[i] > arr[i + 1] :
+                # Left element is greater than that of right in list
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                # Swapping elements of the list
+                swapped = True
+        # Now the smallest element will be in it's correct place in the list
+        begin = begin + 1
 
-     while swapped == True :
-          #so we will continue until it is false
+# Driver Code
 
-          for i in range(begin, end ,1):
-               if arr[i] > arr[i + 1]:
-                    #left element is greater than that of right elemnet in the list
-                    arr[i], arr[i + 1] = arr[i + 1], arr[i]
-                    #swapping the elemnets of the list
-                    swapped = True
-          if swapped == False:
-               break
-               #we will stop as our list is totally sorted
-          swapped = False
+# Sample input arr = [2, 10, 8, 1, 4, 1]
 
-          #now have to apply bubble_sort from opposite direction as our list is not sorted
-          #we have to leave the elemnet at the end as it is in its correct position
-          end = end - 1
-          for i in range(end-1 ,begin - 1,-1):
-               if arr[i] > arr[i + 1]:
-                    #left element is greater than that of right elemnet in the list
-                    arr[i], arr[i + 1] = arr[i + 1], arr[i]
-                    #swapping the elemnets of the list
-                    swapped = True
-          #now the smallest elemnet will be in the left of the list in its correct place
-          #we have to leave that and we have to apply sort on remaining
-          begin = begin + 1
-
-#this is the driver code or tester
-
-#arr=[2, 10, 8, 1, 4, 1]
-#sample input this can be extended over user inputs even
-arr = list()
-arr = list(map(int,input("Enter array: ").strip().split(',')))
+arr = list(map(int,input("Enter Array : ").stript().split(',')))
 Shaker_Sort(arr)
-print("After Shaker_Sort the list :")
-for i in range(0, len(arr), 1):
-     print(arr[i], end = " ")
-#outpu 1 1 2 4 8 10
+print (" After Shaker_Sort the Array : ")
+for i in range (0, len(arr), 1) :
+    print (arr[i], end = " ")
+# Output 1 1 2 4 8 10
+            
+
+
+            
+
+            
+
+     
+     
+
